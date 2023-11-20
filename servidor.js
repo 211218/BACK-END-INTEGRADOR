d63a0fbc9c0fd7e10805bd45ba96dfc5f1a33b83
211218 <211218@ids.upchiapas.edu.mx>
2023-11-19 22:56:48 -0600

    method cors on

--- servidor.js
+++ servidor.js
@@ -1,8 +1,11 @@
 const WebSocket = require('ws');
 const http = require('http');
 const express = require('express');
+const cors = require('cors');
+
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
 
 const wss = new WebSocket.Server({ server, path: '/sensores' });
